d32fa358f0abae76c8c3f32743c6fa4fdd582ebf
RockieKelz <racquy@msn.com>
2022-10-10 17:33:52 -0700

    added main view for text translation

--- app/src/main/java/com/example/mylingual/MainActivity.java
+++ app/src/main/java/com/example/mylingual/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mylingual;
 
 import android.os.Bundle;
+import android.transition.Fade;
+import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Set Up Screen Transition Animation of Activity Page
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        getWindow().setEnterTransition(new Fade());
+        getWindow().setAllowEnterTransitionOverlap(true);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
 
     }
 }
